09bc2442cf37dbcbae6fc848f24ee3adf1ed58d1
yguito <yguqueiroz@alu.ufc.br>
2025-12-03 19:04:09 +0000

    atividade agenda

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -18,11 +18,11 @@ class Fone:
 class Contact:
     def __init__(self, name):
         self.name = name
-         self.fones = []
+        self.fones = []
         self.favorited = False
 
     def addFone(self, id, number):
-        f = Fone(id, number)
+        f =    Fone(id, number)
         if f.isValid():
             self.fones.append(f)
         else:
@@ -69,8 +69,20 @@ class Agenda:
         for f in fones:
             contato.addFone(f.id, f.number)
 
-    def search(self, pa
-            ttern):
+    def rmContact(self, name):
+        pos = self.findPosByName(name)
+        if pos != -1:
+            self.contacts.pop(pos)
+
+    def getFavorited(self):
+        favs = []
+        for c in self.contacts:
+            if c.isFavorited():
+                favs.append(c)
+        return favs
+
+
+    def search(self, pattern):
         res = []
         for c in self.contacts:
             if pattern in str(c):
@@ -84,21 +96,80 @@ class Agenda:
         saida = ""
         for c in self.contacts:
             saida += str(c) + "\n"
-        return saida
+        return saida.strip()
+
+def main():
+    agenda = Agenda()
+    while True:
+        try:
+            line = input()
+            if not line:
+                break
+            if not line.strip():
+                continue
+
+            print(f"${line}" if not line.startswith("$") else line)
+                
+            if line.startswith("$"):
+                line = line[1:]
+
+            parts = line.split()
+            if not parts:
+                continue
+
+            cmd = parts[0]
+
+            if cmd == "end":
+                break
+
+            elif cmd == "add":
+                name = parts[1]
+                fones_args = parts[2:]
+                lista_fones = []
+                for token in fones_args:
+                    dados = token.split(":")
+                    if len(dados) == 2:
+                        lista_fones.append(Fone(dados[0], dados[1]))
+
+                agenda.addContact(name, lista_fones)
+
+            elif cmd == "rmFone":
+                name = parts[1]
+                index =  int(parts[2])
+                pos = agenda.findPosByName(name)
+                if pos != -1:
+                    agenda.contacts[pos].rmFone(index)
+
+            elif cmd == "show":
+                print(agenda)
+            
+            elif cmd == "rm":
+                name = parts[1]
+                agenda.rmContact(name)
+
+            elif cmd == "search":
+                pattern = parts[1]
+                res = agenda.search(pattern)
+                for c in res:
+                    print(c)
+
+            elif cmd == "tfav":
+                name = parts[1]
+                pos = agenda.findPosByName(name)
+                if pos != -1:
+                    agenda.contacts[pos].toggleFavorited()
+
+            elif cmd == "favs":
+                res = agenda.getFavorited()
+                for c in res:
+                    print(c)
+
+        except EOFError:
+                break
+        except Exception as e:
+            print("Deu erro no loop:", e)
+
+if __name__ == "__main__":
+    main()
+
     
-print("teste")
-try:
-    a = Agenda()
-    fones_teste = [Fone("oi", "123")]
-
-    a.addContact("bia", fones_teste)
-    a.addContact("ana", fones_teste)
-
-    print(a)
-except Exception as e:
-    print("deu erro na agenda: ", e)
-
-agenda = Agenda()
-
-while True:
-    line = input("$")
